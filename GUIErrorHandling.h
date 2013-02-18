1fe4a3d5a77488a40d3cbe371313821479b76899
Nathan Daly <NHDaly@gmail.com>
2013-02-18 16:44:44 -0500

    Separated ErrorCatcher classes from GUIError_Handling into GUIError_Handling_Impl.

--- GUIErrorHandling.h
+++ GUIErrorHandling.h
@@ -1,65 +1,27 @@
 //
 //  GUIErrorHandling.h
-//  Deep
+//  GUI Widget Library
 //
 //  Created by Nathan Daly on 12/14/12.
 //  Copyright (c) 2012 Lions Entertainment. All rights reserved.
 //
 
-#ifndef Deep_GUIErrorHandling_h
-#define Deep_GUIErrorHandling_h
+#ifndef GUI_ErrorHandling_h
+#define GUI_ErrorHandling_h
 
-#include <vector>
+#include "GUIError_Handling_Impl.h" // For ErrorCatcher and ErrorCatcher_Impl
 
 namespace GUI {
 
-class ErrorCatcher {
-public:
-    typedef std::vector<ErrorCatcher*>::iterator ErrorCatcherIter_t;
-    virtual void operator()(ErrorCatcherIter_t begin,
-                            ErrorCatcherIter_t end, bool &handled) = 0;
-protected:
-};
-
-
-template <typename Error_t, typename Handler_t>
-class ErrorCatcher_Impl : public ErrorCatcher {
-public:
-    
-    ErrorCatcher_Impl(const Handler_t &handler_) 
-    : handler(handler_)
-    { }
-    
-    virtual void operator()(ErrorCatcherIter_t begin,
-                            ErrorCatcherIter_t end, bool &handled) {
-        
-        // nest a try block for each ErrorCatcher
-        try {
-            if (begin == end) { // base case
-                throw;
-            }
-            ErrorCatcher *next = *begin;
-            (*next)(++begin, end, handled); // unravel until end
-        }
-        // Each ErrorCatcher gets a chance to try to catch.
-        catch(const Error_t &e) {
-            
-            handler(e);     // handler() is only called if Error_t matches.
-            handled = true;
-            throw;
-        }
-    }
-    
-private:
-    Handler_t handler;
-};
-
+// Error_t : The type of errors that will be caught by handler
+// Handler_t : a function or function object that overrides operator()(Error_t);
+// NOTE: handler will be copied.
 template <typename Error_t, typename Handler_t>
 ErrorCatcher* create_error_handler(const Handler_t &handler) {
     return new ErrorCatcher_Impl<Error_t, Handler_t>(handler);
 }
 
-// REQUIRES: This function MUST be called from within a catch(...){} block!
+// REQUIRES: This function MUST be called from within a catch(){} block!
 // Loop through error handlers and handle any errors. If no handler matches
 //  the error, it will be rethrown out of the function.
 template <typename InputIterator>
@@ -72,10 +34,12 @@ void call_error_handlers_helper(InputIterator begin,
                                  InputIterator end, bool handled) {
     
     try {
-        // Create the handlers 
+        // Create a vector of handlers
+        // (Since try_catch is virtual, it cannot be templated,
+        //                                              so it requires a vector.)
         std::vector<ErrorCatcher*> catchers(begin, end);
         
-        (*catchers.front())(++catchers.begin(), catchers.end(), handled);
+        catchers.front()->try_catch(++catchers.begin(), catchers.end(), handled);
         
     }
     catch (...) {
@@ -87,4 +51,4 @@ void call_error_handlers_helper(InputIterator begin,
 
 } // namespace GUI
 
-#endif
+#endif /* GUI_ErrorHandling_h */
