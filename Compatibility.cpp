f735f0d5749513cebf9cb352870b99a7bd5d9a86
Nathan Daly <NHDaly@gmail.com>
2013-07-29 01:39:46 -0400

    added getResource to GUICompatibility

--- Compatibility.cpp
+++ Compatibility.cpp
@@ -9,7 +9,7 @@ void nav_to_resources();
 
 #ifdef _WIN32 // other possibilities are WIN32 _WIN32 or _WIN64
 
-#define MY_RESOURCES_FOLDER "\code"
+const char * MY_RESOURCES_FOLDER = "\code";
 
 void initGUI(){
 
@@ -27,6 +27,13 @@ void nav_to_resources() {
     assert(chdir_success == 0);
 }
 
+const char* getResourcePath() {
+    char *str = new char[strlen(MY_RESOURCES_FOLDER)];
+    strcpy(str, MY_RESOURCES_FOLDER);
+    return (const char*)str;
+}
+
+
 #elif __APPLE__ // Mac OSX
 
 # include <CoreFoundation/CFBundle.h>
@@ -37,9 +44,9 @@ void nav_to_resources() {
 using namespace std;
 
 // For App bundle created in Xcode
-#  define BUNDLE_RESOURCES_FOLDER "/Contents/Resources"
+const char * BUNDLE_RESOURCES_FOLDER = "Contents/Resources";
 // For Standalone executable compiled with Makefile.
-#  define STANDALONE_RESOURCES_FOLDER "/Resources"
+const char * STANDALONE_RESOURCES_FOLDER = "Resources";
 
 void initGUI(){
 	
@@ -47,6 +54,25 @@ void initGUI(){
     
     // Move the working directory to resources folder.
     nav_to_resources();
+}
+
+const char* getResourcePath() {
+    // Get the name of the application bundle
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef url = CFBundleCopyBundleURL(mainBundle);
+    UInt8 bundlePath[PATH_MAX];
+    int chdir_success = (CFURLGetFileSystemRepresentation(url, true, bundlePath, sizeof(bundlePath)));
+    assert(chdir_success != 0);
+
+    cout << "BUNDLE PATH: " << (const char*)bundlePath << endl;
+    // Create the path to the resources folder
+    std::string path((const char*)bundlePath);
+    path.append("/");
+    path.append(BUNDLE_RESOURCES_FOLDER);
+
+    char *str = new char[path.length()+1];
+    strcpy(str, path.c_str());
+    return (const char*)str;
 }
 
 void nav_to_resources() {
@@ -61,6 +87,7 @@ void nav_to_resources() {
     cout << "BUNDLE PATH: " << (const char*)bundlePath << endl;
     // Create the path to the resources folder
     std::string path((const char*)bundlePath);
+    path.append("/");
     path.append(BUNDLE_RESOURCES_FOLDER);
     
     // chdir to the resources folder
@@ -68,6 +95,7 @@ void nav_to_resources() {
     // If failed, try with standalone directory
     if (!(chdir_success == 0)) {
         std::string path((const char*)bundlePath);
+        path.append("/");
         path.append(STANDALONE_RESOURCES_FOLDER);
         
         // chdir to the resources folder
--- Compatibility.h
+++ Compatibility.h
@@ -3,8 +3,12 @@
 #define COMPATABILITY_H
 
 void initGUI();
+const char* getResourcePath(); // note user must free returned c-string
+
 
 #ifdef _MSC_VER // other possibilities are WIN32 _WIN32 or _WIN64
+
+extern const char * MY_RESOURCES_FOLDER;
 
 #include "SDL.h"
 #include "SDL_mixer.h"
@@ -15,6 +19,9 @@ void initGUI();
 #include <direct.h>
 
 #elif __APPLE__ // Mac OSX
+
+extern const char * BUNDLE_RESOURCES_FOLDER;
+extern const char * STANDALONE_RESOURCES_FOLDER;
 
 #include "SDL/SDL.h"
 #include "SDL_ttf/SDL_ttf.h"
